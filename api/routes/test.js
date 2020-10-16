a4591e45e6b6229b2323af861e4d1a7c52c38f97
goaliegamer1 <nicknackluckydog@gmail.com>
2020-10-16 11:21:54 -0400

    Update test.js

--- api/routes/test.js
+++ api/routes/test.js
@@ -39,6 +39,27 @@ router.patch('/:ID', (req, res, next) => {
     })
 });
 
+router.post('/:ID', (req, res, next) => {
+    const id = req.params.ID;
+    AppState.update({_id: id}, {$set: {
+        gods: req.body.gods,
+        relics: req.body.relics,
+        cooldown: req.body.cooldown,
+        objectives: req.body.objectives
+    }})
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    })
+});
+
 router.post('/', (req, res, next) => {
     const appState = new AppState({
         _id: new mongoose.Types.ObjectId(),
